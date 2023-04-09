4f0baf0a1251765b75db2f068172ba767a526cd0
Rubens Brilhante <juniorbrilhante@gmail.com>
2023-04-09 14:46:16 -0300

    Create initial files

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar
+        hidden={false}
+        translucent={false}
+        networkActivityIndicatorVisible={true}
+      />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  MainContainer: {
     justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
   },
 });
